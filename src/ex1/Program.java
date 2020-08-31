a015744f7bbceb7a92a94693a6d5dd0884494699
oh980225 <oh980225@gmail.com>
2020-08-31 22:14:26 +0900

    Start JDBC

---
+++ src/ex1/Program.java
@@ -0,0 +1,21 @@
+package ex1;
+
+import java.sql.Connection;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Connection con;
+
+	}
+
+}
+
+/*
+ 1. 드라이버 로드하기
+ 2. 연결 생성하기
+ 3. 문장 실행하기
+ 4. 결과집합 사용하기 
+*/
+ 
