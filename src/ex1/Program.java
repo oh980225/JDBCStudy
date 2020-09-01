fb7c9371c113bddb73f14d7301269ecece50307c
oh980225 <oh980225@gmail.com>
2020-09-01 14:06:40 +0900

    Connect OracleDB

--- src/ex1/Program.java
+++ src/ex1/Program.java
@@ -1,13 +1,31 @@
 package ex1;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-		Connection con;
-
+		String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+		String sql = "SELECT * FROM NOTICE";
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(url, "JDBC", "admin");
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		
+		if(rs.next()) { // 레코드 하나 가져온다.
+			String title = rs.getString("TITLE"); // 선언한 컬럼의 값을 가져온다.
+			System.out.printf("TITLE : %s\n", title);
+		}
+		
+		rs.close();
+		st.close();
+		con.close();
 	}
 
 }
